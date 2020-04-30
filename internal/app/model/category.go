d013093f2c7f04c4bc803c6e5a50371ad827a2a7
Apostolis Psallas <psallas.apostolis@gmail.com>
2020-04-30 17:20:09 +0300

    Introduce models

--- internal/app/model/category.go
+++ internal/app/model/category.go
@@ -1,3 +1,10 @@
 package model
 
-type Category struct{}
+type Category struct {
+	ID         int    `json:"id"`
+	Title      string `json:"title"`
+	Position   int    `json:"position"`
+	ImageURL   string `json:"image_url"`
+	Created_at string `json:"created_at"`
+	Updated_at string `json:"updated_at"`
+}
--- internal/app/model/product.go
+++ internal/app/model/product.go
@@ -1,3 +1,12 @@
 package model
 
-type Product struct{}
+type Product struct {
+	ID          int     `json:"id"`
+	CategoryID  int     `json:"category_id"`
+	Title       string  `json:"title"`
+	ImageURL    string  `json:"image_url"`
+	Price       float32 `json:"price"`
+	Description string  `json:"description"`
+	Created_at  string  `json:"created_at"`
+	Updated_at  string  `json:"updated_at"`
+}
